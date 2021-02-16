283854fac828075ec272261d468b3ba4a739a79c
wafaankoush99 <wafadirawe@gmail.com>
2021-02-16 23:09:26 +0200

    modified css/style.css & index.html & js/app.js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,42 @@
 
 'use-strict';
 
+let score =0;
+console.log(score);
 
-let userName = prompt ('What is your name? ');
+
+let userName = prompt ( ' What is your name? ' );
 //console.log(userName);
 alert('welcome ' + userName);
 
 
-let myName = prompt ('My name is Wafa? yes/no ');
-//console.log(myName);
 
+
+
+
+
+let myName = prompt ('My name is Wafa? yes/no ');
+///console.log(favDish);
 myName = myName.toLocaleLowerCase(); myName.toLocaleUpperCase();
 
 
 switch (myName) {
-case 'yes':
 case 'y':
-//console.log('Right answer ♥');
+case 'yes':
+//console.log('Right, pizza is my favorite dish');
   alert('Right answer ♥');
+  score++;
   break;
-case 'no':
 case 'n':
-//console.log('I am really Wafa')
+case 'no':
+//console.log('Ops, I love pizza so much');
   alert('I am really Wafa');
   break;
 default:
-//console.log('You must write yes or no')
-  alert('You must write yes or no');
+//console.log('You must write y or n')
+  alert('You must write y or n');
   break;
 }
-
-
 
 
 
@@ -45,6 +51,7 @@ case 'y':
 case 'yes':
 //console.log('Right, pizza is my favorite dish');
   alert('Right, pizza is my favorite dish');
+  score++;
   break;
 case 'n':
 case 'no':
@@ -70,11 +77,14 @@ case 'y':
 case 'yes':
 //console.log('No, not exactly .. I like lightly colours more');
   alert('No, not exactly .. I like lightly colours more');
+
+
   break;
 case 'n':
 case 'no':
 //console.log('Yes, how do you know that ');
   alert('Yes, how do you know that ');
+  score++;
   break;
 default :
 //console.log('You must write y or n')
@@ -95,6 +105,8 @@ case 'y':
 case 'yes':
 //console.log('Yes, I am fond of everything related to art ');
   alert('Yes, I am fond of everything related to art ');
+  score++;
+
   break;
 case 'n':
 case 'no':
@@ -120,6 +132,8 @@ case 'yes':
 case 'y':
   //console.log('I love you toooo ♥ ');
   alert('I love you toooo ♥ ');
+  score++;
+
   break;
 case 'no':
 case 'n':
@@ -134,6 +148,79 @@ default:
 
 
 alert('Thank you ' + userName + ' for your visiting ♥');
+
+
+
+
+
+
+
+
+
+
+
+
+let q1=0;
+console.log(q1);
+for (let i=0 ;i < 4 ; i++)
+{
+  q1 = prompt('How many siblings do I have?');
+  if (Number(q1) > 4) {
+
+    alert('too high');
+
+  }
+  else if (Number(q1) < 4) {
+
+    alert('too low');
+
+  }
+  else if (Number(q1) === 4){
+    alert('right answer');
+    score++;
+
+    break;
+  }
+
+
+}
+
+alert('The correct answer is 4');
+
+
+
+
+
+
+
+let arr1 = ['black' , 'pink', 'grey'];
+console.log(arr1);
+for (let i=0; i < 6 ; i++){
+  let q2 = prompt('what is my favorite color?');
+  q2 = q2.toLocaleLowerCase(); q2.toLocaleUpperCase();
+
+  if( q2 === arr1[0] || q2 === arr1[1] || q2 === arr1[2] ) {
+    alert('correct');
+    score++;
+
+
+    console.log(score);
+
+    break;
+
+  }
+  else {alert('try again');
+  }
+}
+
+alert('the correct answer : black,pink &grey');
+
+alert('your mark is ' + score + ' from 7');
+
+
+
+
+
 
 
 
